554e361e871bb201ef3a31e740832275e6ee2952
kami2 <kami.krasowski.uk@gmail.com>
2023-10-10 22:01:32 +0100

    Add endpoint

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World"
+
+
+@app.route("/")
+def main():
+    enter_text = "Do something..."
+    results = render_template("index.html", enter_text=enter_text)
+    return results
 
 
 if __name__ == "__main__":
