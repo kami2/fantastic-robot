cbba4e2c806bdbe3ce2397978da48e5bcee9de1d
kami2 <kami.krasowski.uk@gmail.com>
2023-10-12 23:03:06 +0100

    Hope that will fix import

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-
-@app.route("/hello")
-def hello():
-    return "Hello World"
-
-
-@app.route("/")
-def main():
-    enter_text = "Do something..."
-    results = render_template("index.html", enter_text=enter_text)
-    return results
-
+from app import app
 
 if __name__ == "__main__":
     app.run()
---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+
+@app.route("/hello")
+def hello():
+    return "Hello World"
+
+
+@app.route("/")
+def main():
+    enter_text = "Do something..."
+    results = render_template("index.html", enter_text=enter_text)
+    return results
