2d288d2bdb37a16e5581f941088231cf6cca7987
kami2 <kami.krasowski.uk@gmail.com>
2023-10-21 15:32:15 +0100

    uptime app test

--- smallworker/routes.py
+++ smallworker/routes.py
@@ -24,8 +24,8 @@ def main():
 @app.route("/app_info")
 def app_info():
     start_time_str = get_config("APP_START_TIME")
-    date_format = "%Y-%m-%d %H:%M:%S"
-    start_time = (datetime.datetime.strptime(start_time_str, date_format)).replace(tzinfo=pytz.UTC)
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    start_time = datetime.datetime.strptime(start_time_str, date_format)
     app_data = {
         "Debug": app.debug,
         "Start Time": str(start_time),
@@ -46,4 +46,3 @@ def generate_prompt():
         ]
     )
     return completion
-
