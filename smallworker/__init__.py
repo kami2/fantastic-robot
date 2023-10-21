e7e6701033330acedcbb489d1d8ee93115358c63
kami2 <kami.krasowski.uk@gmail.com>
2023-10-21 09:48:20 +0100

    Added openai to requirements.txt Changed endpoint: debug_state to app_info Added app start time to app_info

--- smallworker/__init__.py
+++ smallworker/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 import logging
 from smallworker.helpers.route_helper import add_event, generate_image
 from apscheduler.schedulers.background import BackgroundScheduler
+import datetime
 
 app = Flask(__name__)
 app.debug = False
 logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
+start_time = datetime.datetime.now()
 
 # scheduler
 scheduler = BackgroundScheduler(daemon=True)
--- smallworker/routes.py
+++ smallworker/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from smallworker import app
 import requests
+import openai
+from smallworker.utils.config import get_config
+from smallworker import start_time
 
 
 @app.route("/event_store", methods=['GET'])
@@ -16,6 +19,21 @@ def main():
     return results
 
 
-@app.route("/debug_state")
-def debug_state():
-    return {"Debug": app.debug}
+@app.route("/app_info")
+def app_info():
+    return {"Debug": app.debug, "Start Time": str(start_time)}
+
+
+@app.route("/generate_prompt")
+def generate_prompt():
+    openai.api_key = get_config("OPEN_AI_APIKEY")
+    completion = openai.ChatCompletion.create(
+        model="davinci-002",
+        messages=[
+            {"role": "system",
+             "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+            {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        ]
+    )
+    return completion
+
