b88044f412fe67e66b0a3882ad46e5092f1055dd
Kamil <kami.krasowski.uk@gmail.com>
2023-10-23 16:12:45 +0100

    Update __init__.py

--- smallworker/__init__.py
+++ smallworker/__init__.py
@@ -7,14 +7,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 app.debug = False
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s | LEVEL: %(levelname)s | %(message)s',
+                    format='%(asctime)s | LEVEL: %(levelname)s | %(message)s | %(processName)s',
                     handlers=[logging.StreamHandler()])
 
 
-if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(generate_image, 'interval', hours=1)
-    scheduler.start()
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(generate_image, 'interval', hours=1)
+scheduler.start()
 
 
 import smallworker.routes
