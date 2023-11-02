96aa3fe713dba07d5d22c59bcba28fcd3e5268c6
kami2 <kami.krasowski.uk@gmail.com>
2023-11-02 19:46:49 +0000

    Never again small.pl

--- smallworker/routes.py
+++ smallworker/routes.py
@@ -2,6 +2,7 @@ import datetime
 from flask import render_template
 from smallworker import app
 from smallworker.utils.config import get_config
+import logging
 
 
 @app.route("/")
@@ -11,3 +12,8 @@ def main():
     start_time = datetime.datetime.strptime(start_time_str, date_format)
     enter_text = f"Start time {str(start_time)}, Loaded {round((datetime.datetime.utcnow() - start_time).total_seconds() / 3600, 2)}H ago"
     return render_template("index.html", enter_text=enter_text)
+
+
+@app.route("/wake_up", methods=['GET'])
+def wake_up():
+    logging.info("Wake up worker!")
